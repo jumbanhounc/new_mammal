950d3b9d33912217d44f5c74719c49613c919abb
James Umbanhowar <jumbanho@unc.edu>
2021-04-26 15:05:51 -0400

    Got testing working on new directory structure

--- test/test_mammal_data_process.R
+++ test/test_mammal_data_process.R
@@ -1,5 +1,6 @@
-source("cleaner_code.R")
+source("../mammal_data_process.R")
 library(testthat)
+
 
 test_that("Table produced", expect_equal(dim(all_of_it())[1], 7))
 test_that("Length 1 vector", {
